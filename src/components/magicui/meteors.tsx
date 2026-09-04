4c146c73e6f74e063df577b9fb5543ea44366b8c
UnplannedCoder <pawansa2006@gmail.com>
2026-09-04 11:01:49 +0530

    Adding Updated Files

--- src/components/magicui/meteors.tsx
+++ src/components/magicui/meteors.tsx
@@ -1,23 +1,25 @@
-import { cn } from '@/lib/utils'
-import { useEffect, useState } from 'react'
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 interface MeteorsProps {
-  number?: number
-  className?: string
+  number?: number;
+  className?: string;
 }
 
 export const Meteors = ({ number = 20, className }: MeteorsProps) => {
-  const [meteorStyles, setMeteorStyles] = useState<Array<React.CSSProperties>>([])
+  const [meteorStyles, setMeteorStyles] = useState<Array<React.CSSProperties>>(
+    [],
+  );
 
   useEffect(() => {
     const styles = [...new Array(number)].map(() => ({
       top: -5,
-      left: Math.floor(Math.random() * window.innerWidth) + 'px',
-      animationDelay: Math.random() * 1 + 0.2 + 's',
-      animationDuration: Math.floor(Math.random() * 8 + 2) + 's',
-    }))
-    setMeteorStyles(styles)
-  }, [number])
+      left: Math.floor(Math.random() * window.innerWidth) + "px",
+      animationDelay: Math.random() * 1 + 0.2 + "s",
+      animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
+    }));
+    setMeteorStyles(styles);
+  }, [number]);
 
   return (
     <>
@@ -25,7 +27,7 @@ export const Meteors = ({ number = 20, className }: MeteorsProps) => {
         <span
           key={idx}
           className={cn(
-            'pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]',
+            "pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
             "before:absolute before:top-1/2 before:h-[1px] before:w-[50px] before:-translate-y-[50%] before:transform before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-['']",
             className,
           )}
@@ -33,7 +35,7 @@ export const Meteors = ({ number = 20, className }: MeteorsProps) => {
         />
       ))}
     </>
-  )
-}
+  );
+};
 
-import React from 'react'
+import React from "react";
