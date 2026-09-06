fff25e109552c3c4ede258acce11d2056ae8d584
UnplannedCoder <pawansa2006@gmail.com>
2026-09-06 11:01:57 +0530

    Adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from '@/components/sections/Navbar'
-import Hero from '@/components/sections/Hero'
-import About from '@/components/sections/About'
-import Skills from '@/components/sections/Skills'
-import Projects from '@/components/sections/Projects'
-import Certificates from '@/components/sections/Certificates'
-import Contact from '@/components/sections/Contact'
-import Footer from '@/components/sections/Footer'
+import Navbar from "@/components/sections/Navbar";
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
+import Skills from "@/components/sections/Skills";
+import Projects from "@/components/sections/Projects";
+import Certificates from "@/components/sections/Certificates";
+import Contact from "@/components/sections/Contact";
+import Footer from "@/components/sections/Footer";
 
 export default function App() {
   return (
@@ -21,5 +21,5 @@ export default function App() {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
